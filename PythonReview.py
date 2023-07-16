30532d7ba241a303294349ac1c937223db6997c9
katiaam24-meet <139688886+katiaam24-meet@users.noreply.github.com>
2023-07-16 17:17:34 +0300

    Update PythonReview.py

--- PythonReview.py
+++ PythonReview.py
@@ -1,20 +1,22 @@
 def create_youtube_video(title, description):
-	youtube_video = {"title" : title , "description" : description , "likes" : 0 , "dislikes" : 0 , "comments" : {username:""}}
+	youtube_video = {"title" : title , "description" : description , "likes" : 0 , "dislikes" : 0 , "comments" : {'username':""}}
 	return youtube_video
-def like(youtube video):
+def like(youtub_video):
 	if "likes" in youtube_video:
 		likes+=1
 		return youtube_video
 
-def disike(youtube video):
+def disike(youtube_video):
 	if "dislikes" in youtube_video:
 		dislikes+=1
 		return youtube_video
 
-def add_comment(youtube video, username,comment_text):
+def add_comment(youtube_video, username,comment_text):
 	comments ={"username" : comment_text}
 	return comments
 
-new_vid = {making a makloba, yummy}
-	
+new_vid = create_youtube_video('ALIII','ALII WAS HERE')
+print(new_vid)
+
+
 
