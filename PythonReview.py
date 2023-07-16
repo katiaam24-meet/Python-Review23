b1c5d6533f9ebba44247f5047bc21641447a6796
katiaam24-meet <139688886+katiaam24-meet@users.noreply.github.com>
2023-07-16 17:06:51 +0300

    Create PythonReview.py
    python file

---
+++ PythonReview.py
@@ -0,0 +1,20 @@
+def create_youtube_video(title, description):
+	youtube_video = {"title" : title , "description" : description , "likes" : 0 , "dislikes" : 0 , "comments" : {username:""}}
+	return youtube_video
+def like(youtube video):
+	if "likes" in youtube_video:
+		likes+=1
+		return youtube_video
+
+def disike(youtube video):
+	if "dislikes" in youtube_video:
+		dislikes+=1
+		return youtube_video
+
+def add_comment(youtube video, username,comment_text):
+	comments ={"username" : comment_text}
+	return comments
+
+new_vid = {making a makloba, yummy}
+	
+
